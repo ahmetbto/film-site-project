1c405724d0dd0308b90d484383df6f6c076fd600
Ahmet DEMİROK <ahmetbto42@gmail.com>
2022-08-21 14:00:38 +0300

    new

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
+      **{{ count }}
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
@@ -104,9 +103,14 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  computed: {
+    count() {
+      return this.$store.state.test;
+    },
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
 
 export default createStore({
-  state: {},
+  state: {
+    test: 5,
+  },
   mutations: {},
   actions: {},
   modules: {},
